7fa50c4238c1df4cf602d635f2757766c38b5e77
Natalie <natalybolaf2006@gmail.com>
2026-08-12 01:27:13 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,23 +5,20 @@ import About from './components/About/About'
 import Blog from './components/Blog/blog'
 import Contact from './components/Contact/contact'
 import Error from './components/Error/Error'
-import { createHashRouter, RouterProvider } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 export default function App() {
-  let Router = createHashRouter([
-    {
-      path: '/tailwindnat/',
-      element: <Layout />,
-      children: [
-        { index: true, element: <Home /> },
-        { path: 'home', element: <Home /> },
-        { path: 'about', element: <About /> },
-        { path: 'blog', element: <Blog /> },
-        { path: 'contact', element: <Contact /> },
-        { path: '*', element: <Error /> }
-      ]
-    }
-  ])
-
-  return <RouterProvider router={Router} />
+ let Router = createBrowserRouter([{
+    path:"", element:<Layout/>, children: [
+      {path:"/tailwindnat/" , element:<Home/>},
+      {path:"/tailwindnat/home" , element:<Home/>},
+      {path:"/tailwindnat/about" , element:<About/>},
+      {path:"/tailwindnat/blog" , element:<Blog/>},
+      {path:"/tailwindnat/contact" , element:<Contact/>},
+      {path:"/tailwindnat/*" , element:<Error/>}
+    ]
+  }])
+  return <RouterProvider router={Router}/>
 }
+
+
